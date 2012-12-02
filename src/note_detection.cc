9d0495dbb4104f6ff0c54b8b734c523a591cd166
Alexandre Morgand <morgand.alexandre@gmail.com>
2012-12-02 17:13:20 +0100

    note detection: write on image for debug.

--- src/note_detection.cc
+++ src/note_detection.cc
@@ -46,83 +46,95 @@ bool isnote(cv::Mat& img,
 
   // IsWhite and shapped like a square Or Height >> width And Verticale line.
   // FIXME: Hasverticalline doesn't work when the note is upside down.
-  return ((isWhite && abs(diff) < 3) || (diff >= 2.75 || hasverticalline));
+  return (/*(isWhite && abs(diff) < 3) || (diff >= 2.75 ||*/( hasverticalline));
 }
 
 void analyse_note(cv::Mat& img,
                   std::vector<Line>& lines,
                   cv::Mat& note, int x, int y)
 {
+  CvFont font;
+  double hScale = 1.0;
+  double vScale = 1.0;
+  int lineWidth = 1;
+  cvInitFont(&font, CV_FONT_HERSHEY_SIMPLEX | CV_FONT_ITALIC, hScale, vScale, 0, lineWidth);
+
+  //std::string note;
   //display(note, 60);
   unsigned int mid = y + note.size().height / 2;
   // FIXME: not robust if not enough lines.
   unsigned dis;
   int choosen_pitch = -1;
+  std::string notestr = "";
   for (unsigned int i = 0; i < lines.size() - 1; ++i)
   {
-    for (unsigned int x = 0; x < img.size().width * 3; ++x)
-      img.at<uchar>(lines[i].y, x) = 0;
+    /*for (unsigned int x2 = 0; x2 < img.size().width * 3; ++x2)
+      img.at<uchar>(lines[i].y, x) = 0;*/
 
     // FIXME: improvement when note is outside of the line.
     if (mid <= lines[i + 1].y && mid >= lines[i].y)
     {
       dis = lines[i + 1].y - lines[i].y;
-      if (abs(lines[i].y - y) < dis)
+      std::cout << dis << " i: " << abs(lines[i].y - mid) << std::endl;
+      std::cout << dis << " i + 1: " << abs(lines[i + 1].y - mid) << std::endl;
+      if (abs(lines[i].y - mid) < dis / 2)
+      {
+        std::cout << "CHOOSE i !\n";
         choosen_pitch = i % 10;
-      else if (abs(lines[i + 1].y - y) < dis)
-        choosen_pitch = i % 10;
+      }
+      else if (abs(lines[i + 1].y - mid) < dis / 2)
+      {
+        std::cout << "CHOOSE i + 1 !\n";
+        choosen_pitch = (i + 1) % 10;
+      }
     }
 
-    if (mid <= lines[i + 1].y && mid >= lines[i].y)
+    if (choosen_pitch != -1)
     {
-      if (choosen_pitch != -1)
-      {
-        if (choosen_pitch == 0)
-          std::cout << "F" << std::endl;
-        else if (choosen_pitch == 1)
-          std::cout << "D" << std::endl;
-        else if (choosen_pitch == 2)
-          std::cout << "B" << std::endl;
-        else if (choosen_pitch == 3)
-          std::cout << "G" << std::endl;
-        else if (choosen_pitch == 4)
-          std::cout << "F" << std::endl;
+      if (choosen_pitch == 0)
+        notestr = "F";
+      else if (choosen_pitch == 1)
+        notestr = "D";
+      else if (choosen_pitch == 2)
+        notestr = "B";
+      else if (choosen_pitch == 3)
+        notestr = "G";
+      else if (choosen_pitch == 4)
+        notestr = "E";
 
-        else if (choosen_pitch == 5)
-          std::cout << "A" << std::endl;
-        else if (choosen_pitch == 6)
-          std::cout << "F" << std::endl;
-        else if (choosen_pitch == 7)
-          std::cout << "D" << std::endl;
-        else if (choosen_pitch == 8)
-          std::cout << "B" << std::endl;
-        else if (choosen_pitch == 9)
-          std::cout << "G" << std::endl;
-      }
-      else
-      {
-        if (i == 0)
-          std::cout << "E" << std::endl;
-        else if (i == 1)
-          std::cout << "C" << std::endl;
-        else if (i == 2)
-          std::cout << "A" << std::endl;
-        else if (i == 3)
-          std::cout << "F" << std::endl;
-        /*else if (i == 4)
-          std::cout << "C" << std::endl;*/
+      else if (choosen_pitch == 5)
+        notestr = "A";
+      else if (choosen_pitch == 6)
+        notestr = "F";
+      else if (choosen_pitch == 7)
+        notestr = "D";
+      else if (choosen_pitch == 8)
+        notestr = "B";
+      else if (choosen_pitch == 9)
+        notestr = "G";
+    }
+    else
+    {
+      if (i == 0)
+        notestr = "E";
+      else if (i == 1)
+        notestr = "C";
+      else if (i == 2)
+        notestr = "A";
+      else if (i == 3)
+        notestr = "F";
 
-        else if (i == 5)
-          std::cout << "G" << std::endl;
-        else if (i == 6)
-          std::cout << "E" << std::endl;
-        else if (i == 7)
-          std::cout << "C" << std::endl;
-        else if (i == 8)
-          std::cout << "A" << std::endl;
-      }
+      else if (i == 5)
+        notestr = "G";
+      else if (i == 6)
+        notestr = "E";
+      else if (i == 7)
+        notestr = "C";
+      else if (i == 8)
+        notestr = "A";
     }
   }
+  cv::putText(img, notestr, cvPoint(x, y - 20), cv::FONT_HERSHEY_SIMPLEX, 0.4, 255);
 }
 
 void detect_notes(cv::Mat& img,
