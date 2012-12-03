8ea258b98fd379dde00b56408d01d9fe5bdb00e9
Victor Lenoir <lenoir@lrde.epita.fr>
2012-12-03 16:31:29 +0100

    note_detection: add note structure

--- src/note_detection.hh
+++ src/note_detection.hh
@@ -3,14 +3,27 @@
 
 # include "symbol_detection.hh"
 
+enum Pitch
+{
+  PITCH_A,
+  PITCH_B,
+  PITCH_C,
+  PITCH_D,
+  PITCH_E,
+  PITCH_F,
+  PITCH_G
+};
+
 struct Note : public Symbol
 {
-  int note;
+  int octave; // from 0 (grave) to X (aigue)
+  Pitch note; // A,B,C,D,E,F,G
+  int alter; //bemol, becar, diese
 };
 
 void detect_notes(cv::Mat& img,
-		  std::vector<Line>& lines,
-		  std::vector<cv::Rect>& pistes,
-		  std::vector<Symbol>& symbols,
-		  std::vector<Note>& notes);
+                  std::vector<Line>& lines,
+                  std::vector<cv::Rect>& pistes,
+                  std::vector<Symbol>& symbols,
+                  std::vector<Note>& notes);
 #endif // !NOTE_DETECTION_HH_
